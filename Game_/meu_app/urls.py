51f502993e478428474353b4c47e97bcc571e34c
Kauã Goulart <144181448+DevGoulart@users.noreply.github.com>
2025-03-28 15:56:49 -0300

    add Login

--- Game_/meu_app/urls.py
+++ Game_/meu_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('sobre/', views.sobre, name='sobre'),
     path('promocoes/', views.promocoes, name='promocoes'),
     path('login/', views.login, name='login'),
+    path('signup/', views.login, name='signup'),
     path('jogo/<int:jogo_id>/', views.detalhe_jogo, name='detalhe_jogo'),
 ]
--- Game_/meu_app/views.py
+++ Game_/meu_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Jogo
+from django.contrib.auth import authenticate, login as auth_login
+
 def catalogo(request):
     query = request.GET.get('q')  # Pega o texto da busca
     if query:
@@ -11,6 +13,18 @@ def catalogo(request):
 def detalhe_jogo(request, jogo_id):
     jogo = Jogo.objects.get(id=jogo_id)
     return render(request, 'detalhe_jogo.html', {'jogo': jogo})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('catalogo')  # Redireciona para o catálogo após login
+        else:
+            return render(request, 'login.html', {'error': 'Usuário ou senha inválidos'})
+    return render(request, 'login.html', {})
 
 def login(request):
     return render(request, 'login.html')
