28e2c373f30b941a3febb915c40463cdc43e96e4
Nilson Gabriel <116977244+elcariben@users.noreply.github.com>
2025-03-28 10:58:05 -0300

    Update urls.py

--- Game_/meu_app/urls.py
+++ Game_/meu_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.catalogo, name='home'),
+    path('', views.catalogo, name='catalogo'),
     path('jogo/<int:jogo_id>/', views.detalhe_jogo, name='detalhe_jogo'),
 ]
